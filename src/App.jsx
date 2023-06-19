3559c573100563be619acfbd0c00ea055df6c913
Panji <panji9502@gmail.com>
2023-06-19 21:39:21 +0700

    add unsplash code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ThemeToggle from "./ThemeToggle";
+import Gallery from "./Gallery";
+import SearchForm from "./SearchForm";
+
+const App = () => {
+  return (
+    <main>
+      <ThemeToggle />
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
+};
+export default App;
